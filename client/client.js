d7988b4322f7e5c45380c98a44886803df5326ab
sherekhan9 <Sherekhan9@gmail.com>
2024-05-09 00:05:59 -0500

    asdf

--- client/client.js
+++ client/client.js
@@ -3,7 +3,7 @@ RegisterCommand('sayhello', (source, args, rawCommand) => {
     // The args array contains the arguments the player typed after the command
     // args[0] is the first argument, args[1] is the second argument, etc.
     let name = args[0];
-
+    console.log(`Hello, ${name}!` + "TETSING THE BS STUFF");
     // Send a message to the player
     emitNet('chat:addMessage', source, {
         args: [`Hello, ${name}!`]
