d1b977ec26f7f897f506ed02c1b38ed258a11b91
Roids <73410725+Roids-Dev@users.noreply.github.com>
2024-05-08 21:36:24 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'Safecracker Minigame'
+version '1.0.0'
+
+client_scripts {
+  'config.lua',
+  'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
