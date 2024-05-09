15b3f369bb850377c1d447fd219e57b1ffb54426
sherekhan9 <Sherekhan9@gmail.com>
2024-05-09 00:01:38 -0500

    adding command function

---
+++ client/client.js
@@ -0,0 +1,11 @@
+// Register a command 'sayhello' that takes one argument: a name
+RegisterCommand('sayhello', (source, args, rawCommand) => {
+    // The args array contains the arguments the player typed after the command
+    // args[0] is the first argument, args[1] is the second argument, etc.
+    let name = args[0];
+
+    // Send a message to the player
+    emitNet('chat:addMessage', source, {
+        args: [`Hello, ${name}!`]
+    });
+}, false);
