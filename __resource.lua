fdcb715db54e612bb7e0bd1de8c1de501a153091
sherekhan9 <Sherekhan9@gmail.com>
2024-05-08 23:33:56 -0500

    Testing more stuff

---
+++ __resource.lua
@@ -0,0 +1,26 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_scripts {
+	'@es_extended/locale.lua',
+	'client/client.js'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	"@mysql-async/lib/MySQL.lua",
+	'server/server.js',
+	'server/get_name.lua',
+}
+
+
+ui_page('client/html/index.html')
+
+files({
+    'client/html/index.html',
+    'client/html/script.js',
+    'client/html/style.css'
+})
+
+--[[server_scripts {
+	'shared.js',
+	'server/server.js'
+}]]--
