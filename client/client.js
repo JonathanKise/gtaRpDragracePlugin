89f1dabe5fb7cc5c0c78511c59f36f18f2930a42
sherekhan9 <Sherekhan9@gmail.com>
2024-05-09 00:43:24 -0500

    asdf

--- client/client.js
+++ client/client.js
@@ -6,11 +6,13 @@ RegisterCommand('dragrace', (source, args, rawCommand) => {
     console.log("Name: " + args[0]);  
     console.log("Raw Command: " + rawCommand );
 
-    let player = GetPlayerPed();
+    let player = GetPlayerGuid();
     console.log("Player: " + player);
    // let player = GetPlayerName();
    // let playerPos = GetEntityCoords(player, false);
+   const [playerX, playerY, playerZ] = GetEntityCoords(PlayerPedId(), false);
+   console.log(`${playerX}, ${playerY}, ${playerZ}`);
 
-   // let checkpoint = CreateCheckpoint(1, playerPos[0], playerPos[1], playerPos[2], 0, 0, 0, 5, 255, 0, 0, 200, 0);
-
+   let checkpoint = CreateCheckpoint(1, playerPos[0], playerPos[1], playerPos[2], 0, 0, 0, 5, 255, 0, 0, 200, 0);
+ 
 }, false);
