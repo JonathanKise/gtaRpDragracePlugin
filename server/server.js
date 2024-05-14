e54ce8cd81a4e628002797fada6101254da3286d
Roids-Dev <73410725+Roids-Dev@users.noreply.github.com>
2024-05-14 16:53:31 -0500

    e

--- server/server.js
+++ server/server.js
@@ -13,10 +13,12 @@ onNet('dragrace:start', (data) => {
     let testingdata = "Hello E";
     let opponentEndpoint = GetPlayerEndpoint(data.opponentId);
     let wager = data.wager;
+    let playerD = QBCore.Functions.GetPlayer(data.playerSrc);
+    let opponentD = QBCore.Functions.GetPlayer(data.opponentId);
     console.log(opponentEndpoint);
     console.log(data.playerSrc);
     console.log(data.opponentId);
-    console.log(data.playerSrc.PlayerData.money.cash)
+    console.log(playerD.PlayerData.money.cash);
     if (!opponentEndpoint || data.opponentId == data.playerSrc) {
         //INVALID OPPONENT MESSAGE GOES HERE
         emitNet('dragrace:testing', data.playerSrc, "Invalid Opponet/Invalid ID type")
